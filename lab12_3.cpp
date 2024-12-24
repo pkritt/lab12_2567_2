cd44990e85c5963872205536d7bd28583d358df9
pkritt <krittapat37819@gmail.com>
2024-12-25 00:21:55 +0700

    fin

--- lab12_3.cpp
+++ lab12_3.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
 
+using namespace std;
+
+void mySwap(int &,int &); 
+
+int main(){
+	int x,y;
+	cin >> x >> y;
+	cout << "Before swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	mySwap(x,y);
+	cout << "After swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	
+	return 0;
+}
+
+void mySwap(int &x,int&y){
+    int z;
+    z = y;
+    y = x;
+    x = z;
+}
