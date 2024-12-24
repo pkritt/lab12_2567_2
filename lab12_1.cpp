9c93392d33b4641300f85a0e46ff85914f32393d
pkritt <krittapat37819@gmail.com>
2024-12-25 00:03:11 +0700

    fin

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+
+long long int fibonacci(int n);
+
+int main(){
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+long long int fibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
