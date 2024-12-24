b9eec671c5934da2d84afa1593e291951a57a441
pkritt <krittapat37819@gmail.com>
2024-12-25 00:35:48 +0700

    fin

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+template <class T>
+T  mySwap(T&,T&){ 
+    T z;
+    z = y; 
+    y = x;
+    x = z;
+}
+
+int main(){
+	int x, y;
+	string a, b;
+	char p, q;
+	
+	cin >> x >> y >> a >> b >> p >> q;
+	
+	cout << "Before swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	mySwap(x,y);
+	cout << "After swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	
+	cout << "Before swapping:\n";
+	cout << "a = " << a << ", b = " << b << "\n";
+	mySwap(a,b);
+	cout << "After swapping:\n";
+	cout << "a = " << a << ", b = " << b << "\n";
+	
+	cout << "Before swapping:\n";
+	cout << "p = " << p << ", q = " << q << "\n";
+	mySwap(p,q);
+	cout << "After swapping:\n";
+	cout << "p = " << p << ", q = " << q << "\n";
+
+	return 0;
+}
 
